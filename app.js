dc96d8703abbb0cd11df87b024e944bfe50edf11
Jesnine Erillo <jesninenica@yahoo.com>
2019-12-28 00:42:36 -0800

    NEW and CREATE routes

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ const Blog = mongoose.model('Blog', blogSchema)
 
 app.get('/', (req, res) => res.redirect('/blogs'))
 
+// INDEX ROUTE
 app.get('/blogs', (req, res) => {
     Blog.find({}, (err, blogs) => {
         if (err) {
@@ -32,5 +33,20 @@ app.get('/blogs', (req, res) => {
     })
 })
 
+// NEW ROUTE
+app.get('/blogs/new', (req, res) => {
+    res.render('new')
+})
+
+// CREATE ROUTE
+app.post('/blogs', (req, res) => {
+    Blog.create(req.body.blog, (err, newBlog) => {
+        if (err) {
+            res.render('new')
+        } else {
+            res.redirect('/blogs')
+        }
+    })
+})
 
 app.listen(3000, () => console.log('Server is running'))
